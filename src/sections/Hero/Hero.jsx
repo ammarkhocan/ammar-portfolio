2e162325f02fbcf65bd97d678d7bac9ab0fb2bfc
ammae khocan <ammarkhocan02@gmail.com>
2025-01-27 20:03:44 +0700

    build Hero

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,18 +1,27 @@
 import style from "./HeroStyle.module.css";
 import heroImg from "../../assets/hero-img.png";
-import themeIcon from "../../assets/sun.svg";
-import twitterIcon from "../../assets/twitter-light.svg";
-import githubIcon from "../../assets/github-light.svg";
-import linkedinIcon from "../../assets/linkedin-light.svg";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
 import CV from "../../assets/cv.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
 
+  const themeIcon = theme === "light" ? sun : moon;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+
   return (
     <section id="hero" className={style.container}>
-      <div className={style.colorModelContainer}>
+      <div className={style.colorModeContainer}>
         <img className={style.hero} src={heroImg} alt="Profil picture" />
         <img
           className={style.colorMode}
@@ -22,9 +31,7 @@ function Hero() {
         />
       </div>
       <div className={style.info}>
-        <h1>
-          Ammar Ismail <br /> Khocan
-        </h1>
+        <h1>Ammar Ismail Khocan</h1>
         <h2>Front End Developer</h2>
         <span>
           <a href="" target="_blank">
@@ -37,7 +44,7 @@ function Hero() {
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
-        <p>
+        <p className={style.description}>
           With a passion developing modern React web apps for commercial
           businesses.
         </p>
